374b2e53320445269ff70c594745436e36425cd2
unnati-wq <105584596+unnati-wq@users.noreply.github.com>
2022-08-06 22:48:50 +0530

    Add files via upload

--- processor/PropertyProcessor.java
+++ processor/PropertyProcessor.java
@@ -1,5 +1,6 @@
 package edu.upenn.cit594.processor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,25 +9,51 @@ import edu.upenn.cit594.util.PropertiesData;
 
 public class PropertyProcessor {
 	
-	int avgMV;
-	int avgLA;
-	int MVperCapita;
+	long avgMV = 0;
+	long avgLA;
+	long MVperCapita;
 	
-	public int avgMarketValue(String FileName) {
+	public long avgMarketValue(String FileName, String zipCode) {
 		PropertiesReader read = new PropertiesReader(FileName);
 		Map<String, PropertiesData> PropertiesDataMap=new HashMap<String,PropertiesData>();
-		
+		PropertiesDataMap = read.read();
+		for (String zip : PropertiesDataMap.keySet()) {
+			if(zip.equals(zipCode)) {
+				PropertiesData data = PropertiesDataMap.get(zip);
+				ArrayList <PropertiesData> propdata = new ArrayList <PropertiesData> ();
+				propdata = data.getdata();
+				long totalprop = 0;
+				long mv;
+				long sum = 0;
+				for(int i = 0; i<propdata.size(); i++) {
+					PropertiesData inddata = propdata.get(i);
+					mv = inddata.getMarketValue();
+					if(mv == 0) {
+						continue;
+					}
+					else {
+						totalprop++;
+						sum = sum + mv; 
+					}
+					
+				}
+				avgMV = sum/totalprop;
+				return avgMV;
+			}
+			
+		}
 		return avgMV;
+		
 		
 	}
 	
-	public int avgLivableArea() {
+	public long avgLivableArea() {
 		
 		return avgLA;
 		
 	}
 	
-	public int MVperCapita() {
+	public long MVperCapita() {
 		
 		return MVperCapita;
 		
